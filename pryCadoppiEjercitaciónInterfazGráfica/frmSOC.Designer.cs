e9a080ffcfb9fa18754fc2b19b91e50d3363a7bd
JuanCruzCadoppi <juancruzcadoppi03@hotmail.com>
2026-08-18 11:48:45 -0300

    errores arreglados

--- pryCadoppiEjercitaciónInterfazGráfica/frmSOC.Designer.cs
+++ pryCadoppiEjercitaciónInterfazGráfica/frmSOC.Designer.cs
@@ -32,9 +32,6 @@
             lblAlto = new Label();
             lblMedio = new Label();
             lblBajo = new Label();
-            this.lblResultadoBajo = new Label();
-            this.lblResultadoMedio = new Label();
-            this.lblResultadoAlto = new Label();
             lblResultadoCritico = new Label();
             SuspendLayout();
             // 
@@ -75,36 +72,6 @@
             lblBajo.Text = "BAJO";
             lblBajo.Click += lblBajo_Click;
             // 
-            // lblResultadoBajo
-            // 
-            this.lblResultadoBajo.AutoSize = true;
-            this.lblResultadoBajo.Location = new Point(132, 144);
-            this.lblResultadoBajo.Name = "lblResultadoBajo";
-            this.lblResultadoBajo.Size = new Size(12, 15);
-            this.lblResultadoBajo.TabIndex = 8;
-            this.lblResultadoBajo.Text = "-";
-            this.lblResultadoBajo.Click += this.label1_Click_1;
-            // 
-            // lblResultadoMedio
-            // 
-            this.lblResultadoMedio.AutoSize = true;
-            this.lblResultadoMedio.Location = new Point(132, 111);
-            this.lblResultadoMedio.Name = "lblResultadoMedio";
-            this.lblResultadoMedio.Size = new Size(12, 15);
-            this.lblResultadoMedio.TabIndex = 7;
-            this.lblResultadoMedio.Text = "-";
-            this.lblResultadoMedio.Click += this.label2_Click;
-            // 
-            // lblResultadoAlto
-            // 
-            this.lblResultadoAlto.AutoSize = true;
-            this.lblResultadoAlto.Location = new Point(132, 76);
-            this.lblResultadoAlto.Name = "lblResultadoAlto";
-            this.lblResultadoAlto.Size = new Size(12, 15);
-            this.lblResultadoAlto.TabIndex = 6;
-            this.lblResultadoAlto.Text = "-";
-            this.lblResultadoAlto.Click += this.label3_Click;
-            // 
             // lblResultadoCritico
             // 
             lblResultadoCritico.AutoSize = true;
@@ -113,16 +80,12 @@
             lblResultadoCritico.Size = new Size(12, 15);
             lblResultadoCritico.TabIndex = 5;
             lblResultadoCritico.Text = "-";
-            lblResultadoCritico.Click += this.label4_Click;
             // 
             // frmSOC
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(249, 203);
-            Controls.Add(this.lblResultadoBajo);
-            Controls.Add(this.lblResultadoMedio);
-            Controls.Add(this.lblResultadoAlto);
+            ClientSize = new Size(226, 231);
             Controls.Add(lblResultadoCritico);
             Controls.Add(lblBajo);
             Controls.Add(lblMedio);
